7a41bb199f7070bde28ed2ed9d75a14450a1ef1e
Yvad <yvesyvad@gmail.com>
2022-01-14 00:20:37 +0200

    creating routes as separate files

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
-
 const app = express()
-
 const PORT = process.env.PORT || 5000
 
+//ROUTES
+app.get('/', (req, res) => {
+  res.send('We are on home')
+})
+
 app.listen(PORT, () => {
-  console.log('Server Started');
+  console.log(`Server running on port https://localhost:${PORT}`);
 })
