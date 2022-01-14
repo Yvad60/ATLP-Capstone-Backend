4f165e0b2fdf2e705370dc91884c44bdae9768e7
Yvad <yvesyvad@gmail.com>
2022-01-14 08:26:16 +0200

    adding files

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+// importing dependecies
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 5000
 
-//ROUTES
+dotenv.config()
+
 app.get('/', (req, res) => {
   res.send('We are on home')
 })
 
+mongoose.connect(process.env.MONGODB_URL)
+
 app.listen(PORT, () => {
-  console.log(`Server running on port https://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 })
