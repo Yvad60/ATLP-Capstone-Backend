0ae8b8f1a02354115d572659ddc8caacd3425035
Yvad <yvesyvad@gmail.com>
2022-01-17 00:21:01 +0200

    adding appropriate routes methods

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,7 @@
 const articleModel = require('../models/blog')
 const express = require('express')
 const app = module.exports = express()
+const bodyParser = require('body-parser')
 
 
 // Read for the blog CRUD operation 
@@ -17,11 +18,11 @@ app.get('/blog', (req, res) => {
 
 //Delete  for the CRUD operation
 //delete the article by it's id
-app.get('/blog/delete', (req, res) => {
-  articleModel.findByIdAndDelete(id)
+app.delete('/blog/delete/:articleId', (req, res) => {
+  articleModel.findByIdAndDelete(req.params.articleId)
     .then((result) => {
       if (!result) {
-        res.send(`article with id ${id} can't be found`)
+        res.send(`article with id ${req.params.articleId} can't be found`)
       }
       else {
         res.send('article deleted successfully')
@@ -29,12 +30,15 @@ app.get('/blog/delete', (req, res) => {
     })
 })
 
+app.patch
+
+
 //Create for the CRUD operation 
 //create a new article based on the schema
-app.get('/blog/add', (req, res) => {
+app.post('/blog/add', (req, res) => {
   const newBlog = new articleModel({
     title: 'Buy the PS4 While You still can',
-    author: 'Yvad Yves',
+    author: 'Yvad jean',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
   })
   newBlog.save()
