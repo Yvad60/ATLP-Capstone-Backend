c77653bfe7f39be96e09fdabbc020d2953ce8828
Yvad <yvesyvad@gmail.com>
2022-01-19 11:30:02 +0200

    Add home route

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express';
 import connectMongo from './dbConfing.js';
-import mainRouter from './routes/index.js';
+import {mainRouter, homeRoute} from './routes/index.js';
 
 const PORT = process.env.PORT || 5000
 const app = express();
 
 app.use(express.json());
 app.use('/api/v1', mainRouter)
+app.get("/", homeRoute)
 
 
 app.listen(PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,28 @@ import blogRouter from "./blog.js";
 mainRouter.use('/blogs', blogRouter);
 // mainRouter.use('auth/', auth);
 // mainRouter.use('/users', usersRouter)
+const welcomeMessage =
+  `
+<head>
+<title>Home</title>
+<style>
+body {
+  display :flex;
+  text-align: center;
+  align-items: center;
+  color:black;
+  justify-content: center;
+  flex-direction: column
 
-export default mainRouter;
+
+}
+</style>
+</head>
+<h1>Welcome home</h1><br> <hr>
+<h3>I am Ivad</h3> <br> <hr>
+<p>Go to the docs <a href='/docs'>Here</a>
+`
+export const homeRoute = async (req, res) => {
+  return res.status(200).send(welcomeMessage)
+}
+export { mainRouter };
