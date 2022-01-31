ca72be82dd3fa4c13defc9cb54ffffdfe1810342
Yvad <yvesyvad@gmail.com>
2022-01-31 08:11:24 +0200

    fixed response errors

--- controllers/messages.js
+++ controllers/messages.js
@@ -20,10 +20,10 @@ const getAllMessages = async (req, res) => {
   const messages = await messageModel.find()
   try {
     if (messages) {
-      return res.status(200).json(handleResponse('success', 200, messages))
+      return res.status(200).json(messages)
     }
     else {
-      return res.status(404).json(handleResponse('fail', 404, { message: 'no messages found' }))
+      return res.status(404).json({ message: 'no messages found' })
     }
   } catch (error) {
     return res.status(500).json(handleResponse('fail', 500, { message: 'error on our servers' }))
